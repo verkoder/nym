9ea4ad42047cb9b2997d32d620dc9ba9967311c9
verkoder <61431864+verkoder@users.noreply.github.com>
2020-02-28 15:16:47 -0500

    table css

--- spin/tables.py
+++ spin/tables.py
@@ -10,13 +10,12 @@ class CommonTable(tables.Table):
         exclude = ('num_vote_up', 'num_vote_down', 'vote_score')
 
     def render_name(self, value, record):
-        return format_html(f'<a href="/{self.Meta.model.__name__.lower()}/{record.pk}">{value}</a>')
+        return format_html(f'<a href="/{self.Meta.model.__name__.lower()}/{record.pk}"><b>{value}</b></a>')
 
     def render_id(self, value):
         return format_html(
-            f'<span style="white-space: nowrap;"><a '
-            f'href="/{self.Meta.model.__name__.lower()}/update/{value}">&check;</a><a '
-            f'href="/{self.Meta.model.__name__.lower()}/delete/{value}">&timesb;</a></span>'
+            f'<a href="/{self.Meta.model.__name__.lower()}/update/{value}">&check;</a><br>'
+            f'<a href="/{self.Meta.model.__name__.lower()}/delete/{value}">&timesb;</a> '
         )
 
 class QuadranymTable(CommonTable):
@@ -24,11 +23,11 @@ class QuadranymTable(CommonTable):
         model = Quadranym
         attrs = {'class': 'commons'}
         fields = ('id', 'name', 'area', 'src', 'user', 'realm', 'e', 'r', 'o', 's',
-                   'pos', 'epos', 'rpos', 'opos', 'spos', 'wiki')
+                  'pos', 'epos', 'rpos', 'opos', 'spos', 'wiki')
 
 class PolynymTable(CommonTable):
     class Meta:
         model = Polynym
         attrs = {'class': 'commons'}
         fields = ('id', 'name', 'area', 'src', 'user', 'mode', 'depth', 'p1', 'p2', 'p3',
-                   'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'wiki')
+                  'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'wiki')
