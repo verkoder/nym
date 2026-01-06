20a23e6fc1239933eb704ffed3551e5a7191e317
Scotty Vercoe <61431864+verkoder@users.noreply.github.com>
2026-01-05 22:47:38 -0500

    Delete keys.py

--- keys.py
+++
@@ -1,5 +0,0 @@
-DJANGO_SECRET = 'n*-e1tg4+o!=63w6^_(a*m$-r&=73-q3z3bxc4hpful47gc&34'
-
-NYMBASE_PASSWORD = 'numberbatch617'
-
-QUOTES_API = 'https://www.stands4.com/services/v2/quotes.php?uid=7528&tokenid=0O9HSQuPUVTHvOmP&searchtype=SEARCH&format=json&query='
