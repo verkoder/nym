c9590b694e234f722981861b742b2c77bc7bcf2b
Scotty Vercoe <61431864+verkoder@users.noreply.github.com>
2026-01-05 22:47:57 -0500

    Delete _etc directory

--- _etc/urls.py
+++
@@ -1,2 +0,0 @@
-path('plot/poly_tilemap/',          views.plot_poly_tilemap,   name='plot_poly_tilemap'),   # P-depths mode: tilemap,lol
-path('qontext/',                    views.qontext_vu,     name='qontext'), # QONTEXT
